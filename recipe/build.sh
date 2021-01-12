7f311372f9bfba3387c4f752b90396fe63e20ff9
Uwe L. Korn <uwe.korn@quantco.com>
2021-01-12 14:10:45 +0100

    Overwrite location of protoc plugin to support cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,8 @@ pushd build_cmake
 
 export OPENSSL_ROOT_DIR=$PREFIX
 
+# Overwrite location of protoc plugin to support cross-compilation
+sed -ie "s;protoc-gen-grpc.*$;protoc-gen-grpc=${BUILD_PREFIX}/bin/grpc_cpp_plugin;g" ../cmake/CompileProtos.cmake
 cmake ${CMAKE_ARGS} -GNinja \
     -DBUILD_TESTING=OFF \
     -DBUILD_SHARED_LIBS=ON \
